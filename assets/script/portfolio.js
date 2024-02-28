cf5db3884de1984eab6e75ff5c153388293cf92c
Fellipe C.T De Camargo <fellipectcamargo@gmail.com>
2024-02-28 16:29:37 -0400

    Personal Portfolio Website
    First commit

---
+++ assets/script/portfolio.js
@@ -0,0 +1,31 @@
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+let title = document.getElementById("title");
+let scrollWidth = scrollContainer.clientWidth;
+
+window.addEventListener("resize", () => {
+  location.reload();
+  scrollContainer.scrollLeft = 0;
+  console.log(window.innerHeight);
+  // if (window.innerHeight < 550) {
+  //   title.style.marginTop = "100%";
+  // }
+
+});
+
+scrollContainer.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+  scrollContainer.style.scrollBehavior = "auto";
+});
+
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += scrollWidth;
+});
+
+backBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= scrollWidth;
+});
---
+++ assets/script/script.js
@@ -0,0 +1,23 @@
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+
+function openTab(tabName) {
+  for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].classList.remove("active-link");
+  }
+  for (i = 0; i < tabContents.length; i++) {
+    tabContents[i].classList.remove("active-tab");
+  }
+
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabName).classList.add("active-tab");
+}
+
+function openMenu() {
+  var sideMenu = document.getElementById("sideMenu");
+  sideMenu.style.right = "0";
+}
+function closeMenu() {
+  var sideMenu = document.getElementById("sideMenu");
+  sideMenu.style.right = "-160px";
+}
